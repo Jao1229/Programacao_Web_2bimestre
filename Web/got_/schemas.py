f5a369e3172b9d622e09b2dc298601b97b6d27b5
Jao1229 <162514087+Jao1229@users.noreply.github.com>
2026-06-15 21:33:10 -0300

    Delete Web/got_/schemas.py

--- Web/got_/schemas.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel, Field
-
-class SolicitacaoServico(BaseModel):
-    cuidador_id_gateway: str = Field(..., description="ID da conta do cuidador no gateway")
-    valor_total: float = Field(..., description="Valor total cobrado em Reais")
-    token_cartao: str = Field(..., description="Token do cartão gerado pelo Frontend")
